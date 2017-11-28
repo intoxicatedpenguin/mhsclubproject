c550c19e97f2c4daec936aa74a4bc481639ba477
Raveneus <raveneus.cwillguy@gmail.com>
2017-11-28 15:15:37 -0600

    Changed collision detection to use collide2D

--- collideable.js
+++ collideable.js
@@ -5,42 +5,15 @@ function Collideable(x, y, w, h, shape) {
     this.h = h;
     this.shape = shape;
 
-    this.distance = function(otherElement) {
-        var dx = this.x - otherElement.x;
-        var dy = this.y - otherElement.y;
-        var distance = Math.sqrt(dx * dx + dy * dy);
-        return distance;
-    }
-
     this.collide = function (otherElement) {
         if (otherElement.shape == "rect" && this.shape == "ellipse") { // this is a circle
-            var distance = this.distance(otherElement);
-            if (distance < this.w) { // collision
-                return true;
-            }
-            return false;
+            return collideRectCircle(otherElement.x, otherElement.y, otherElement.w, otherElement.h, this.x, this.y, this.w);
         } else if (otherElement.shape == "ellipse" && this.shape == "rect") { // this is a block
-            var distance = this.distance(otherElement);
-            if (distance < otherElement.w) { // collision
-                return true;
-            }
-            return false;
+            return collideRectCircle(this.x, this.y, this.w, this.h, otherElement.x, otherElement.y, otherElement.w);
         } else if (otherElement.shape == "rect" && this.shape == "rect") {
-            if (this.x < otherElement.x + otherElement.width &&
-                this.x + this.width > otherElement.x &&
-                this.y < otherElement.y + otherElement.height &&
-                this.height + this.y > otherElement.y) {
-                 // collision detected!
-                 return true;
-             }
-             return false;
+            return collideRectRect(this.x, this.y, this.w, this.h, otherElement.x, otherElement.y, otherElement.w, otherElement.h);
         } else if (otherElement.shape == "ellipse" && this.shape == "ellipse") {
-            var distance = this.distance(otherElement);
-            
-            if (distance < this.w + otherElement.w) {
-                return true;// collision detected!
-            }
-            return false;
+            return collideCircleCircle(this.x, this.y, this.w, otherElement.x, otherElement.y, otherElement.w);
         }
     }
     this.show = function() {
